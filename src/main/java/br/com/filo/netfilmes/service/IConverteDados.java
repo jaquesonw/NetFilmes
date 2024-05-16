03f65ac4648969cc90a009505764bce8d6688613
jaqueson <jaquesonw@gmail.com>
2024-05-16 17:11:12 -0300

    primeiro projeto spring

---
+++ src/main/java/br/com/filo/netfilmes/service/IConverteDados.java
@@ -0,0 +1,5 @@
+package br.com.filo.netfilmes.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+}
---
+++ src/test/java/br/com/filo/netfilmes/NetfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filo.netfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
